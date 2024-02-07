c3d84f605c31bfcb35de5763102315a8e4141628
Christian <habinezac15@gmail.com>
2024-02-07 10:11:36 +0200

    ft: composition technique added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,8 @@
 import ListBox from './ListBox';
 import WatchedBox from './WatchedBox';
 
-const Main = ({ movies }) => {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+const Main = ({ children }) => {
+  return <main className="main">{children}</main>;
 };
 
 export default Main;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,8 @@ import Logo from './Logo';
 import Search from './Search';
 import NumResults from './NumResults';
 
-const NavBar = ({ movies }) => {
-  return (
-    <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResults movies={movies} />
-    </nav>
-  );
+const NavBar = ({ children }) => {
+  return <nav className="nav-bar">{children}</nav>;
 };
 
 export default NavBar;
